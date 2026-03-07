94eb254c4f0d2dbc978affb53be57e48db95c1c8
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2026-03-07 11:05:36 +0530

    [Aryaman]: added UC1 - Create Contacts

--- src/main/java/com/addressbookapp/AddressbookappApplication.java
+++ src/main/java/com/addressbookapp/AddressbookappApplication.java
@@ -3,13 +3,21 @@ package com.addressbookapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.addressbookapp.model.Contact;
+
 @SpringBootApplication
 public class AddressbookappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AddressbookappApplication.class, args);
-		
+
 		System.out.println("Welcome to Address Book");
+
+		Contact contact = new Contact("Aryaman", "Kushwaha", "MP Nagar", "Bhopal", "Madhya Pradesh", "462001",
+				"9876543210", "aryaman@email.com");
+
+		System.out.println(contact);
+
 	}
 
 }
