6f0ba16d9f6a1da794d397e8b54c6043acc9c484
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2026-03-07 11:42:31 +0530

    [Aryaman]: added UC2 - Add a Contact in Address Book

--- src/main/java/com/addressbookapp/AddressbookappApplication.java
+++ src/main/java/com/addressbookapp/AddressbookappApplication.java
@@ -1,9 +1,12 @@
 package com.addressbookapp;
 
+import java.util.Scanner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.addressbookapp.model.Contact;
+import com.addressbookapp.services.AddressBook;
 
 @SpringBootApplication
 public class AddressbookappApplication {
@@ -13,11 +16,40 @@ public class AddressbookappApplication {
 
 		System.out.println("Welcome to Address Book");
 
-		Contact contact = new Contact("Aryaman", "Kushwaha", "MP Nagar", "Bhopal", "Madhya Pradesh", "462001",
-				"9876543210", "aryaman@email.com");
+		Scanner scanner = new Scanner(System.in);
 
-		System.out.println(contact);
+		AddressBook addressBook = new AddressBook();
 
+		System.out.println("Enter First Name:");
+		String firstName = scanner.nextLine();
+
+		System.out.println("Enter Last Name:");
+		String lastName = scanner.nextLine();
+
+		System.out.println("Enter Address:");
+		String address = scanner.nextLine();
+
+		System.out.println("Enter City:");
+		String city = scanner.nextLine();
+
+		System.out.println("Enter State:");
+		String state = scanner.nextLine();
+
+		System.out.println("Enter Zip:");
+		String zip = scanner.nextLine();
+
+		System.out.println("Enter Phone Number:");
+		String phone = scanner.nextLine();
+
+		System.out.println("Enter Email:");
+		String email = scanner.nextLine();
+
+		Contact person = new Contact(firstName, lastName, address, city, state, zip, phone, email);
+
+		addressBook.addContact(person);
+
+		System.out.println("\nContacts in Address Book:");
+		addressBook.displayContacts();
 	}
 
 }
